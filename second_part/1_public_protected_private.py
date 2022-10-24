1699d6d0f9774e79c4e3dd890e429c0e54c489e8
Valexandrin <alex-vadim@mail.ru>
2022-10-24 20:11:32 +0300

    ex 6, 7, 8 added

--- second_part/1_public_protected_private.py
+++ second_part/1_public_protected_private.py
@@ -37,3 +37,87 @@ class Money:
     def add_money(self, obj):
         if self.__check_money(obj.__money):
             self.__money += obj.__money
+
+
+#ex6
+class Book:
+    def __init__(self, author, title, price) -> None:
+        self.__author = author
+        self.__title = title
+        self.__price = price
+
+    def set_title(self, title):
+        self.__title = title
+
+    def set_author(self, author):
+        self.__author = author
+
+    def set_price(self, price):
+        self.__price = price
+
+    def get_title(self):
+        return self.__title
+
+    def get_author(self):
+        return self.__author
+
+    def get_price(self):
+        return self.__price
+
+
+#ex7
+class Line:
+    def __init__(self, *args) -> None:
+        self.__x1, self.__y1, self.__x2, self.__y2 = args
+
+    def set_coords(self, *args):
+        self.__x1, self.__y1, self.__x2, self.__y2 = args
+
+    def get_coords(self):
+        return self.__x1, self.__y1, self.__x2, self.__y2
+
+    def draw(self):
+        print(*self.get_coords())
+
+
+#ex8
+class Point:
+    def __init__(self, x, y):
+        if self.__check_coords(x) and self.__check_coords(y):
+            self.__x = x
+            self.__y = y
+
+    def __check_coords(cls, val):
+        if isinstance(val, (int, float)):
+            return True
+        return False
+
+    def get_coords(self) -> tuple:
+        return self.__x, self.__y
+
+
+
+class Rectangle:
+    def __init__(self, *args) -> None:
+        if len(args) == 4:
+            x1, y1, x2, y2 = args
+            self.__sp = Point(x1, y1)
+            self.__ep = Point(x2, y2)
+        else:
+            self.__sp = args[0]
+            self.__ep = args[1]
+
+
+    def set_coords(self, sp, ep):
+        self.__sp = sp
+        self.__ep = ep
+
+    def get_coords(self):
+        return self.__sp, self.__ep
+
+    def draw(self):
+        print("Прямоугольник с координатами: {coords} ".format(
+            coords = self.get_coords(),
+        ))
+
+rect = Rectangle(0, 0, 20, 34)
