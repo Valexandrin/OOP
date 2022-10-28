db5333f2f9c5ad2010e8942b1f1885f86ebdb630
Valexandrin <alex-vadim@mail.ru>
2022-10-28 17:09:31 +0300

    ex8 added

--- second_part/2_property.py
+++ second_part/2_property.py
@@ -167,3 +167,61 @@ class RadiusVector2D:
     def y(self, y):
         if self.__check_coord(y):
             self.__y = y
+
+
+#ex8
+class TreeObj:
+    def __init__(self, indx: int, value=None) -> None:
+        self.indx = indx
+        self.value = value
+        self.__left = None
+        self.__right = None
+
+    @property
+    def left(self):
+        return self.__left
+
+    @left.setter
+    def left(self, obj):
+        self.__left = obj
+
+    @property
+    def right(self):
+        return self.__right
+
+    @right.setter
+    def right(self, obj):
+        self.__right = obj
+
+class DecisionTree:
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def predict(cls, root: TreeObj, x: list):
+        while not root.value:
+            if x[root.indx]:
+                root = root.left
+            else:
+                root = root.right
+        return root.value
+
+    @classmethod
+    def add_obj(cls, obj: TreeObj, node: TreeObj=None, left=True):
+        if node:
+            if left:
+                node.left = obj
+            else:
+                node.right = obj
+        return obj
+
+root = DecisionTree.add_obj(TreeObj(0))
+v_11 = DecisionTree.add_obj(TreeObj(1), root)
+v_12 = DecisionTree.add_obj(TreeObj(2), root, False)
+DecisionTree.add_obj(TreeObj(-1, "будет программистом"), v_11)
+DecisionTree.add_obj(TreeObj(-1, "будет кодером"), v_11, False)
+DecisionTree.add_obj(TreeObj(-1, "не все потеряно"), v_12)
+DecisionTree.add_obj(TreeObj(-1, "безнадежен"), v_12, False)
+
+x = [1, 1, 0]
+res = DecisionTree.predict(root, x)
